f985ae59a528105429d7ec8757a4e8a0841430b6
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-08 11:41:33 -0500

    adjsuted buttons and added ratings

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 // Global variables
-var topics = ["World of Warcraft", "Overwatch", "League of Legends", "Dota 2", "Fortnite", "Smashbros", "Mariokart?", "Runescape", "Pokemon"];
+var topics = ["World of Warcraft", "Overwatch", "League of Legends", "Dota 2", "Fortnite", "Smashbros", "Mariokart", "Runescape", "Pokemon"];
 
 $(document).ready(function () {
     function gameTitles () {
         $("#buttonArea").empty();
         for (i = 0; i < topics.length; i++) {
             var button = $("<button>");
-            button.addClass("button game").attr("title", topics[i]).text(topics[i]);
+            button.addClass("btn btn-secondary game m-1").attr("title", topics[i]).text(topics[i]);
             $("#buttonArea").append(button);
         }
     }
@@ -24,7 +24,7 @@ $(document).ready(function () {
     function populateGifs () {
         $("#gifHoldingArea").empty();
         var game = $(this).attr("title");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=dc6zaTOxFJmzC&limit=10"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=dc6zaTOxFJmzC&limit=25"
     
     $.ajax({
         url: queryURL,
@@ -32,9 +32,13 @@ $(document).ready(function () {
     }).then(function(response) {
         console.log(response);
         for (i = 0; i < response.data.length; i++) {
+            var gifDiv = $("<div>");
             var image = $("<img>");
+            var rating = $("<h3>")
+            rating.text(response.data[i].rating);
             image.attr("src", response.data[i].images.fixed_height.url);
-            $("#gifHoldingArea").append(image);
+            gifDiv.append(rating).append(image);
+            $("#gifHoldingArea").prepend(gifDiv);
         }
     })
         
