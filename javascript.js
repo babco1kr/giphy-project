8f52f9f99c664a0fb416ce128266ddd0a2d90680
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-08 18:40:31 -0500

    done

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // Global variables
-var topics = ["World of Warcraft", "Overwatch", "League of Legends", "Dota 2", "Fortnite", "Smashbros", "Mariokart", "Runescape", "Pokemon"];
+var topics = ["World of Warcraft", "Overwatch", "League of Legends", "Dota 2", "Fortnite", "Smashbros", "Mariokart", "Runescape", "Pokemon", "Call of Duty", "Counter Strike Go"];
 
 $(document).ready(function () {
     function gameTitles () {
@@ -24,7 +24,7 @@ $(document).ready(function () {
     function populateGifs () {
         $("#gifHoldingArea").empty();
         var game = $(this).attr("title");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=dc6zaTOxFJmzC&limit=25"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=dc6zaTOxFJmzC&limit=10"
     
     $.ajax({
         url: queryURL,
@@ -36,12 +36,29 @@ $(document).ready(function () {
             var image = $("<img>");
             var rating = $("<h3>")
             rating.text(response.data[i].rating);
-            image.attr("src", response.data[i].images.fixed_height.url);
+            image.addClass("gif");
+            image.attr("src", response.data[i].images.fixed_height_still.url).attr("game-animate", response.data[i].images.fixed_height.url);
+            image.attr("game-still", response.data[i].images.fixed_height_still.url).attr("motionstatus", "still");
             gifDiv.append(rating).append(image);
             $("#gifHoldingArea").prepend(gifDiv);
         }
     })
         
     }
+
+    $("#gifHoldingArea").on("click", ".gif", function() {
+        var motion = $(this).attr("motionstatus");
+        console.log(motion);
+
+        if (motion === "still") {
+            $(this).attr("src", $(this).attr("game-animate"));
+            $(this).attr("motionstatus", "animate");
+            
+        }
+        else {
+            $(this).attr("src", $(this).attr("game-still"));
+            $(this).attr("motionstatus", "still");
+        }
+    })
 
 })
